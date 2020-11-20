e9d00add77b26b974b25675cab1ecd0cf1735e15
Idong Essien <idong.essien@comcast.net>
2020-11-20 16:57:10 -0500

    Added code to play drum sound on button click

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ let btn = document.querySelectorAll('.drum');
 
 for (let b = 0; b < btn.length; b++) {
     btn[b].addEventListener('click', () => {
-        alert('I got clicked !!');
+        new Audio('./sounds/tom-1.mp3').play();
     });
 }
 
 let updateDate = new Date().getFullYear();
 document.querySelector('#crdate').append(updateDate);
+
+
+// let audio = new Audio('');
+// audio.play();
