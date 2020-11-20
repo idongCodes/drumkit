fa8c48c405c66f71d788091fe0347da0e9f8a727
Idong Essien <idong.essien@comcast.net>
2020-11-20 14:54:19 -0500

    Changed footer. Added javascript to update year in footer.

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ for (let b = 0; b < btn.length; b++) {
         alert('I got clicked !!');
     });
 }
+
+let updateDate = new Date().getFullYear();
+document.querySelector('#crdate').append(updateDate);
