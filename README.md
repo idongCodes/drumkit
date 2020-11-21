38085154c20863db404bb1a3c42c387a6e7298c2
Idong Essien <idong.essien@comcast.net>
2020-11-20 22:47:53 -0500

    Added an about page that explains the project.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # drumkit
+To help deepen my understanding of DOM:
+- I used JS to target button elements
--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 let btn = document.querySelectorAll('.drum');
 
 for (let b = 0; b < btn.length; b++) {
-    btn[b].addEventListener('click', () => {
-        new Audio('./sounds/tom-1.mp3').play();
+
+    btn[b].addEventListener('click', function() {
+        // new Audio('./sounds/tom-1.mp3').play();
+        var btnInnerHTML = this.innerHTML;
+        switch (btnInnerHTML) {
+            case "w":
+                new Audio('./sounds/tom-1.mp3').play();
+                break;
+            
+            case "a":
+                new Audio('./sounds/tom-2.mp3').play();
+                break;
+
+            case "s":
+                new Audio('./sounds/tom-3.mp3').play();
+                break;
+
+            case "d":
+                new Audio('./sounds/tom-4.mp3').play();
+                break;
+
+            case "j":
+                new Audio('./sounds/snare.mp3').play();
+                break;
+
+            case "k":
+                new Audio('./sounds/kick-bass.mp3').play();
+                break;
+
+            case "l":
+                new Audio('./sounds/crash.mp3').play();
+                break;
+        
+            default:
+                break;
+        }
     });
+
 }
 
 let updateDate = new Date().getFullYear();
